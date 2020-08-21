ce5aa0db6028647095ab89d336746302ed1b8e07
DELINSKI ROMAN <Wendigo91@inbox.ru>
2020-08-21 12:03:21 +0300

    bug fix

--- js/search.js
+++ js/search.js
@@ -1,35 +1,41 @@
 const searchWindow = async (event) => {
-    event.preventDefault();
-    const x = $("#search").val();
-    window.location.replace(`/search.html?${x}`);
-
+  event.preventDefault();
+  const x = $("#search").val();
+  window.location.replace(`/search.html?${x}`);
 };
 
-
 $(document).ready(async () => {
-    let search = window.location.search;
-    let clearURI = decodeURI(search)
-    const clear = clearURI.substr(1);
-    const itemMovie = await fetch(`https://api.themoviedb.org/3/search/movie?query=${clear}&api_key=ad2fb2e9ab12851bd813fca1a20c373e&language=en-US&page=1&include_adult=false&page=1`)
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (data) {
-            return data;
-        })
-        .catch(function (error) {
-            console.log('error', error)
-        })
-    let item = itemMovie.results
-    await item.map((data) => {
-        $('.loading').hide()
-        $('.search_item_box').append(`
+  let search = window.location.search;
+  let clearURI = decodeURI(search);
+  const clear = clearURI.substr(1);
+  const itemMovie = await fetch(
+    `https://api.themoviedb.org/3/search/movie?query=${clear}&api_key=ad2fb2e9ab12851bd813fca1a20c373e&language=en-US&page=1&include_adult=false&page=1`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      return data;
+    })
+    .catch(function (error) {
+      console.log("error", error);
+    });
+  let item = itemMovie.results;
+  await item.map((data) => {
+    $(".loading").hide();
+    $(".search_item_box").append(`
             <div class="search_item">
-                <img src="https://image.tmdb.org/t/p/w500/${data.poster_path ? data.poster_path : ""}"
-                    alt="No Photo">
+                <img src=${
+                  data.poster_path
+                    ? `https://image.tmdb.org/t/p/w500/${data.poster_path}`
+                    : "https://image.tmdb.org/t/p/w500//oKjivUsUQ0tx9zEEUvqgDCK2Gnw.jpg"
+                }
+                    alt="">
                 <div class="review">
                     <div class="review_title">
-                        <a href="/films.html?${data.id}">${data.title}<a><span>${data.release_date ? data.release_date.substr(0, 4) : ""}</span>
+                        <a href="/films.html?${data.id}">${
+      data.title
+    }<a><span>${data.release_date ? data.release_date.substr(0, 4) : ""}</span>
                     </div>
                     <div class="description_title">
                         <p>This review may contain spoilers.</p>
@@ -39,20 +45,19 @@ $(document).ready(async () => {
                     </div>
                 </div>
             </div>
-        `)
-    })
-    if (item.length > 0) {
-        $('.result_title').append(`
+        `);
+  });
+  if (item.length > 0) {
+    $(".result_title").append(`
         <h1>FOUND AT LEAST ${item.length} MATCHES FOR "${clear}"</h1>
-    `)
-    } else {
-        $('.result_title').append(`
+    `);
+  } else {
+    $(".loading").hide();
+    $(".result_title").append(`
         <h1>NOT FOUNT RESULTS</h1>
-    `)
-        $('.result_content').append(`
+    `);
+    $(".result_content").append(`
         <h1>There were no matches for your search term.</h1>
-        `)
-    }
-
-
-})
+        `);
+  }
+});
