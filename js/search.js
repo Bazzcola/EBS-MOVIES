9b3741ac7b1cab84dccc6c59633f610d01bbee9a
DELINSKI ROMAN <Wendigo91@inbox.ru>
2020-08-21 18:28:25 +0300

    loader fix

--- js/search.js
+++ js/search.js
@@ -7,7 +7,8 @@ const searchWindow = async (event) => {
 $(document).ready(async () => {
   let search = window.location.search;
   let clearURI = decodeURI(search);
-  const clear = clearURI.substr(1);
+  let clear = clearURI.substr(1);
+
   const itemMovie = await fetch(
     `https://api.themoviedb.org/3/search/movie?query=${clear}&api_key=ad2fb2e9ab12851bd813fca1a20c373e&language=en-US&page=1&include_adult=false&page=1`
   )
@@ -21,43 +22,55 @@ $(document).ready(async () => {
       console.log("error", error);
     });
   let item = itemMovie.results;
-  await item.map((data) => {
+  try {
+    await item.map((data) => {
+      $(".loading").hide();
+      $(".search_item_box").append(`
+              <div class="search_item">
+                  <img src=${
+                    data.poster_path
+                      ? `https://image.tmdb.org/t/p/w500/${data.poster_path}`
+                      : "https://image.tmdb.org/t/p/w500//oKjivUsUQ0tx9zEEUvqgDCK2Gnw.jpg"
+                  }
+                      alt="">
+                  <div class="review">
+                      <div class="review_title">
+                          <a href="/films.html?${data.id}">${
+        data.title
+      }<a><span>${
+        data.release_date ? data.release_date.substr(0, 4) : ""
+      }</span>
+                      </div>
+                      <div class="description_title">
+                          <p>This review may contain spoilers.</p>
+                      </div>
+                      <div class="likes">
+                          <p>${data.vote_count} like's</p>
+                      </div>
+                  </div>
+              </div>
+          `);
+    });
+    if (item.length > 0) {
+      $(".result_title").append(`
+          <h1>FOUND AT LEAST ${item.length} MATCHES FOR "${clear}"</h1>
+      `);
+    } else {
+      $(".loading").hide();
+      $(".result_title").append(`
+          <h1>NOT FOUNT RESULTS</h1>
+      `);
+      $(".result_content").append(`
+          <h1>There were no matches for your search term.</h1>
+          `);
+    }
+  } catch (error) {
     $(".loading").hide();
-    $(".search_item_box").append(`
-            <div class="search_item">
-                <img src=${
-                  data.poster_path
-                    ? `https://image.tmdb.org/t/p/w500/${data.poster_path}`
-                    : "https://image.tmdb.org/t/p/w500//oKjivUsUQ0tx9zEEUvqgDCK2Gnw.jpg"
-                }
-                    alt="">
-                <div class="review">
-                    <div class="review_title">
-                        <a href="/films.html?${data.id}">${
-      data.title
-    }<a><span>${data.release_date ? data.release_date.substr(0, 4) : ""}</span>
-                    </div>
-                    <div class="description_title">
-                        <p>This review may contain spoilers.</p>
-                    </div>
-                    <div class="likes">
-                        <p>${data.vote_count} like's</p>
-                    </div>
-                </div>
-            </div>
-        `);
-  });
-  if (item.length > 0) {
     $(".result_title").append(`
-        <h1>FOUND AT LEAST ${item.length} MATCHES FOR "${clear}"</h1>
-    `);
-  } else {
-    $(".loading").hide();
-    $(".result_title").append(`
-        <h1>NOT FOUNT RESULTS</h1>
-    `);
+          <h1>NOT FOUNT RESULTS</h1>
+      `);
     $(".result_content").append(`
-        <h1>There were no matches for your search term.</h1>
-        `);
+          <h1>There were no matches for your search term.</h1>
+          `);
   }
 });
